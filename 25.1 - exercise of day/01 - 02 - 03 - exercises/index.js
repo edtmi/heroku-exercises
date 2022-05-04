e3d8119e4757fbfd8c219064ebafbc08cbf55df2
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-05-04 20:15:13 -0300

    putting exercise 3 responses

--- 25.1 - exercise of day/01 - 02 - 03 - exercises/index.js
+++ 25.1 - exercise of day/01 - 02 - 03 - exercises/index.js
@@ -16,3 +16,17 @@ app.get('/', (_req, res) => {
 app.listen(PORT, () => {
   console.log(`App escutando na porta ${PORT}`);
 });
+
+/*
+Exercício 3. Simule o deploy em multi-ambientes (produção e homologação). Para isso:
+
+Renomeie o remote do deploy dos exercícios anteriores para homolog;
+RESP: git remote rename heroku homolog
+
+Crie um novo remote a partir do mesmo projeto. Dessa vez, o remote deverá se chamar prod;
+RESP: heroku create --remote prod
+      git remote rename heroku prod
+Em seguida, configure as variáveis de ambiente para terem valores diferentes por ambiente.
+RESP: heroku config:set NEW_TEXT="texto do HOMOLOG" --app stormy-chamber-32594
+      heroku config:set NEW_TEXT="texto do PROD" --app intense-oasis-66481  
+*/
